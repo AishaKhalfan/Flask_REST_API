cdd5c2a2c44dda46cdd5fb90c4db943b1937a33d
AishaKhalfan <aishakhalfan45@gmail.com>
2023-06-24 22:13:50 +0300

    RequestParser

--- test.py
+++ test.py
@@ -2,6 +2,17 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response  = requests.get(BASE + "helloworld/Aisha")
-#response  = requests.post(BASE + "helloworld")
+data = [{"likes": 40, "name": "aisha", "views": 50000},
+        {"likes": 78, "name": "How to make REST API", "views": 10000},
+        {"likes": 100, "name": "Learn all about ARGS PARSER", "views": 70000}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response  = requests.get(BASE + "video/2")
 print(response.json())
